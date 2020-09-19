eb195efecb44e358a761762e1ca24979854d5017
fahim abrar <fahimabrar13@gmail.com>
2020-09-19 15:24:07 +0600

    solved

--- FirstSolution/MobileBill/MobileBill/Program.cs
+++ FirstSolution/MobileBill/MobileBill/Program.cs
@@ -11,30 +11,51 @@ namespace MobileBill
             DateTime start = DateTime.Parse(Console.ReadLine());
             Console.Write("End time:");
             DateTime end = DateTime.Parse(Console.ReadLine());
-            double rate = 0;
-            if (IsPeak(start, end))
-                rate = 30;
-            else
-                rate = 20;
-            double seconds = (end - start).TotalSeconds;
-            double pulse = seconds / 20.0;
-            Console.WriteLine((pulse * rate)/100.0);
+            var peaks = IsPeak(start,end);
+
+            Console.WriteLine($"{((double)peaks.Item1*30.0+(double)peaks.Item2*20.0)/100.00} taka");
 
         }
 
-        static bool IsPeak(DateTime start,DateTime end)
+        static (int,int) IsPeak(DateTime start,DateTime end)
         {
+            int offpick = 0;
+            int onpick = 0;
             DateTime peakstartDate = DateTime.Parse("2019-08-31 9.00.00 am");
             var peakstart = peakstartDate.TimeOfDay;
             DateTime peakendDate = DateTime.Parse("2019-08-31 10.59.59 pm");
             var peakend = peakendDate.TimeOfDay;
             var startday = start.TimeOfDay;
             var endday = end.TimeOfDay;
-            if (startday >= peakstart && startday <= peakend)
-                return true;
-            if (endday >= peakstart && endday <= peakend)
-                return true;
-            return false;
+            if (startday >= peakstart && startday <= peakend && endday >= peakstart && endday <= peakend)
+            {
+                double seconds = (end - start).TotalSeconds;
+                onpick =(int)seconds / 20;
+                return (onpick, offpick);
+            }
+            else if (startday >= peakstart && startday <= peakend)
+            {
+                double tot = (end - start).TotalSeconds;
+                double seconds = (peakend - startday).TotalSeconds;
+                double totalpeak = Math.Ceiling(tot / 20);
+                onpick = (int)Math.Ceiling(seconds / 20)+1;
+                offpick = (int)totalpeak - onpick;
+                return (onpick, offpick);
+            }else if(endday >= peakstart && endday <= peakend)
+            {
+                double tot = (end - start).TotalSeconds;
+                double seconds = (endday - peakstart).TotalSeconds;
+                double totalpeak = Math.Ceiling(tot / 20);
+                onpick = (int)Math.Ceiling(seconds / 20)+1;
+                offpick = (int)totalpeak - onpick;
+                return (onpick, offpick);
+            }
+            else
+            {
+                double seconds = (end - start).TotalSeconds;
+                offpick = (int)seconds / 20;
+                return (onpick, offpick);
+            }
         }
     }
 }
